f1f9ade95a5ff2a283e84b6e3f9657ef192dcdc3
Mark Pampuch <markpampuch@gmail.com>
2023-08-18 23:57:42 -0400

    tweak layout

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -51,8 +51,19 @@ export default function BasicTabs() {
   };
 
   return (
-    <Box sx={{ width: "100%" }} className="mt-auto">
+    <Box sx={{ width: "100%" }} className="">
       <Box>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+          centered
+          className="flex justify-between"
+        >
+          <Tab label="DNA Input" {...a11yProps(0)} />
+          <Tab label="Enzyme Selection" {...a11yProps(1)} />
+          <Tab label="Extra Parmeters" {...a11yProps(2)} />
+        </Tabs>
         <CustomTabPanel value={value} index={0}>
           <h1>DNA input</h1>
           <p>
@@ -60,10 +71,10 @@ export default function BasicTabs() {
             has to be in FASTA format and be less than 1Mb.
           </p>
           {/* TODO, FIX THE OVERFLOW AND ADD A SCROLL WHEN THIS GETS TOO BIG */}
-          <div className="scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-10 max-h-[300px] overflow-auto">
+          <div className="scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-10 max-h-[250px] overflow-auto">
             <TextareaAutosize
               aria-label="minimum height"
-              minRows={3}
+              minRows={10}
               placeholder="Input DNA to digest"
               className="text-gray-800"
             />
@@ -72,9 +83,18 @@ export default function BasicTabs() {
             Upload File
             <input type="file" hidden />
           </Button>
+          <LabelledSwitch />
+          {/* TODO, ADD THIS LOGIC TO THE LABELLED DNA INPUT THING BUT DO IT USING A GLOBAL STATE MANAGEMENT SYSTEM LIKE REDUX TO ENSURE PARENT CAN GET THE STATE */}
+          <Switch
+            onChange={() =>
+              setShowRestrictedInput(
+                (showRestrictedInput) => !showRestrictedInput,
+              )
+            }
+          />
           <div
-            className={`overflow-hidden transition-all duration-500 ease-in-out ${
-              showRestrictedInput ? "max-h-[300px]" : "max-h-0"
+            className={`overflow-auto transition-all duration-500 ease-in-out ${
+              showRestrictedInput ? "max-h-[450px]" : "max-h-0"
             }`}
           >
             {/* {showRestrictedInput && (
@@ -89,9 +109,9 @@ export default function BasicTabs() {
             <div className="scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-10 max-h-[300px] overflow-auto">
               <TextareaAutosize
                 aria-label="minimum height"
-                minRows={3}
+                minRows={10}
                 placeholder="Input DNA that you don't want to digest"
-                className="text-gray-800"
+                className="max-h-[250px] text-gray-800"
               />
             </div>
             <Button variant="contained" component="label">
@@ -101,15 +121,6 @@ export default function BasicTabs() {
             {/* </>
             )} */}
           </div>
-          <LabelledSwitch />
-          {/* TODO, ADD THIS LOGIC TO THE LABELLED DNA INPUT THING BUT DO IT USING A GLOBAL STATE MANAGEMENT SYSTEM LIKE REDUX TO ENSURE PARENT CAN GET THE STATE */}
-          <Switch
-            onChange={() =>
-              setShowRestrictedInput(
-                (showRestrictedInput) => !showRestrictedInput,
-              )
-            }
-          />
         </CustomTabPanel>
         <CustomTabPanel value={value} index={1}>
           <h1>Enzyme Selection</h1>
@@ -118,17 +129,6 @@ export default function BasicTabs() {
           <h1>Extra parameters</h1>
           <Button variant="contained">Default</Button>
         </CustomTabPanel>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-          centered
-          className="flex justify-between"
-        >
-          <Tab label="DNA Input" {...a11yProps(0)} />
-          <Tab label="Enzyme Selection" {...a11yProps(1)} />
-          <Tab label="Extra Parmeters" {...a11yProps(2)} />
-        </Tabs>
       </Box>
     </Box>
   );
