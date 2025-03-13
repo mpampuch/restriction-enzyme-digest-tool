b8fe1753632f95bda65609f87eaf795d28de0b33
Mark Pampuch <markpampuch@gmail.com>
2025-03-13 15:14:23 +0300

    allow all hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,9 @@ export default defineConfig({
   server: {
     host: "0.0.0.0", // Allow external connections to the server (e.g. from the host machine). By using 0.0.0.0 instead of the default 127.0.0.1 in your Dockerized services, you ensure that they are accessible from both inside and outside the Docker container. This is crucial for applications that need to interact with other services or users across a network, facilitating easier debugging, testing, and integration of Dockerized applications.
     port: 3000, // Replace with your desired port number
+    allowedHosts: "all",
   },
   define: {
-      'process.env.ENCODING': JSON.stringify('utf-8'), // Override encoding
-    },
+    "process.env.ENCODING": JSON.stringify("utf-8"), // Override encoding
+  },
 });
