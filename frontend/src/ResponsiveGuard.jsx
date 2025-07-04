160d797733c2a96f11acbf42cb32583e6a846258
Mark Pampuch <markpampuch@gmail.com>
2025-07-04 17:31:53 +0300

    Make responsive

---
+++ frontend/src/ResponsiveGuard.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+const MOBILE_BREAKPOINT = 768; // px
+
+export default function ResponsiveGuard({ children }) {
+  const [isMobile, setIsMobile] = useState(
+    typeof window !== "undefined" && window.innerWidth < MOBILE_BREAKPOINT,
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  if (isMobile) {
+    return (
+      <div
+        style={{
+          minHeight: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#222",
+          color: "#fff",
+          fontSize: "1.5rem",
+          textAlign: "center",
+          padding: "2rem",
+        }}
+      >
+        This application is only available on desktop-sized screens. <br />
+        Please use the correct device or enlarge your browser window.
+      </div>
+    );
+  }
+  return children;
+}
+
+ResponsiveGuard.propTypes = {
+  children: PropTypes.node,
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import ResponsiveGuard from "./ResponsiveGuard";
 
 import { Provider } from "react-redux";
 import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ResponsiveGuard>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ResponsiveGuard>
   </React.StrictMode>,
 );
